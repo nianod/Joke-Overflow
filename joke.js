ebe0657676361dd5bb3d2fd4f8f7c94b73e1c42c
nianod <arnoldkk422@gmail.com>
2025-04-08 10:27:15 +0300

    final

--- joke.js
+++ joke.js
@@ -1,20 +1,22 @@
 const jokeDisplay = document.getElementById("showJoke")
-const getJoke = document.getElementById("showJoke")
+const getJoke = document.getElementById("getJoke")
 const jokeApi = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
 
 const joke = () => {
     fetch(jokeApi)
-    .then((resolve, reject) => {
-        console.log(resolve);
-        let data = resolve.json();
-        console.log(data);
-        
-        
+    .then(resolve => resolve.json()) 
+    .then(data => {
+
+        jokeDisplay.textContent = `${data.joke}`
+
     })
+
     // .catch(reject) {
     //     console.log("You experienced a network failiure");
         
     // }
 }
+
+getJoke.addEventListener("click", joke)
 
 joke();
